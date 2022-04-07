9adb7da7120f055c0c273fe9b6dc90a7db8f7e3b
wilderiverab14 <100375242+wilderiverab14@users.noreply.github.com>
2022-04-07 13:28:03 -0500

    zertrhrts

---
+++ src/practica.js
@@ -0,0 +1,21 @@
+/*javascript como todo lenguaje de programacion se basa en la utilizacion de palabras preservadas*/
+//creacion de variables
+/*var nom = "wilder";//no se recomienda usarla
+const nombre = "WILDER RIVERA";
+let apellido = "bustamante";
+
+alert (nombre);
+*/
+
+let numero = 3;
+let numero1 = 2;
+let suma = numero + numero1;
+print (suma);
+
+/* operaciones aritmeticos en javascript
++ aficion
+- resta
+* multiplicacion
+/ division
+% residuo
+*/
